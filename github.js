8b9ba8ab9675bde448884f7da3d339f9e1a5c587
Bogdan <98848790+bogdanveljovic@users.noreply.github.com>
2022-04-15 12:29:39 +0200

    Add files via upload

---
+++ github.js
@@ -0,0 +1,21 @@
+class Github {
+    constructor() {
+      this.client_id = 'c88b713e84d0631a2938';
+      this.client_secret = '470a8321df80a4ace23b0186a78339317f08b3b9';
+      this.repos_count = 5;
+      this.repos_sort = "created : asc";
+    }
+  
+    async getUser(user) {
+      const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+      
+      const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+      const profile = await profileResponse.json();
+      const repos = await repoResponse.json()
+       
+      return {
+        profile,
+        repos
+      }
+    }
+}
